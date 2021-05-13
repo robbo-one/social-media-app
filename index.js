7f93b8a27f5722d7f875f5c76e53a5f9979a0ead
Rob <robsc801@gmail.com>
2021-05-13 14:32:09 +1200

    config and cluster connection

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+
+const { MONGODB } = require('./config.js')
 
 const typeDefs = gql`
     type Query{
@@ -18,7 +22,12 @@ const server = new ApolloServer({
     resolvers
 })
 
-server.listen({ port: 5000})
-    .then(res => {
-        console.log(`server running at ${res.url}`)
+mongoose
+        .connect(MONGODB, { useNewUrlParser: true })
+        .then(() => {
+                console.log('MongoDB connected')
+            return server.listen({ port: 5000 })
+        })
+        .then(res => {
+            console.log(`Server running at ${res.url}`)
     })
