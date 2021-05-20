72318b0a17ada704c43001b4a539c9fc979124ec
Rob <robsc801@gmail.com>
2021-05-20 16:00:56 +1200

    make sure username isn't taken

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { UserInputError } = require('apollo-server')
 
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
@@ -16,7 +17,17 @@ module.exports = {
         ){
             // TODO: validate user data
             // TODO: Make sure user doesnt already exist
-            // TODO: hash password and create an auth token
+
+            const user = await User.findOne({ username })
+            if(user){
+                throw new UserInputError('Username is taken', {
+                errors: {
+                    username: 'This username is taken'
+                } 
+              })
+            }
+
+            // : hash password and create an auth token
             password = await bcrypt.hash(password, 12)
 
             const newUser = new User({
