e7758ff09d922385dda39c195d7defa1eec19c3f
Rob <robsc801@gmail.com>
2021-05-20 14:48:26 +1200

    create sign in token etc.

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,44 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jswebtoken')
+
+const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
 module.exports = {
     Mutation: {
-        register(_, args, context, info){
+        async register(
+            _,
+             {
+                registerInput : { username, email, password, confirmPassword}
+        }, 
+        context, 
+        info
+        ){
             // TODO: validate user data
             // TODO: Make sure user doesnt already exist
             // TODO: hash password and create an auth token
-            
+            password = await bcrypt.hash(password, 12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save()
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET+KeyboardEvent, { expiresIn: '1h'})
+
+            return{
+                ...res._doc,
+                id: res._id,
+                token
+            }
         }
     }
 }
