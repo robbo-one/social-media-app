91917b092eaa3379b2a2e2018a44b5117dcb5261
Rob <robsc801@gmail.com>
2021-05-21 15:44:45 +1200

    login input validator working

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,15 +2,13 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 
-const {
-     validateRegisterInput, validateLoginInput
-     } = require('../../util/validators')
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
 function generateToken(user){
     return jwt.sign(
-        {
+        { 
         id: user.id,
         email: user.email,
         username: user.username
@@ -19,21 +17,26 @@ function generateToken(user){
        { expiresIn: '1h'}
        )
 }
-
+ 
 module.exports = {
     Mutation: {
-        async login(_, { username, password }){
-            const {errors, valid} = validateLoginInput(username,password)
+        async login(_, { username, password }) {
+            const { errors, valid } = validateLoginInput(username,password)
+            
+             if (!valid) {
+                throw new UserInputError('Errors', { errors })
+            }
+
             const user = await User.findOne({ username })
 
-            if(!user){
+             if (!user) {
                 errors.general = 'User not found'
                 throw new UserInputError('User not found', { errors })
              }
 
-            const match = await bcrypt.compare(password, user, password)
-            if(!match){
-                errors.general = 'Wrong credentils'
+            const match = await bcrypt.compare(password, user.password)
+            if(!match) {
+                errors.general = 'Wrong credentials'
                 throw new UserInputError('Wrong credentials', { errors })
             }
 
@@ -86,15 +89,8 @@ module.exports = {
 
             const res = await newUser.save()
 
-            const token = jwt.sign(
-            {
-              id: res.id,
-              email: res.email,
-              username: res.username
-            }, SECRET_KEY,
-             { expiresIn: '1h'}
-             )
-
+            const token = generateToken(res)
+            
             return{
                 ...res._doc,
                 id: res._id,
--- util/validators.js
+++ util/validators.js
@@ -36,4 +36,9 @@ module.exports.validateRegisterInput = (
          if (password.trim() === '') {
             errors.password = 'Password must not be empty'
     }
-    }
+    return {
+        errors,
+        valid: Object.keys(errors).length < 1
+      };
+    };
+    
