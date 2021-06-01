5cbb079d40f84b9be16c05eeb89bfd943629cebb
Rob <robsc801@gmail.com>
2021-06-01 17:39:02 +1200

    wire in components to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css'
 import "./App.css";
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/login' component={Login}/>
+      <Route exact path='register' component={Register}/>
+    </Router>
   );
 }
 
