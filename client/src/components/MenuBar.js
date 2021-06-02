e66d8f1e2a828e46a0f1138fccb0bc1f50a70bc0
Rob <robsc801@gmail.com>
2021-06-02 12:31:26 +1200

    change menu button to highlight current page, teal color

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom'
 
 function MenuBar() {
   
-    const [activeItem, setActiveItem] = useState('')
+    const pathname = window.location.pathname
+ 
+    const path = pathname === '/' ? 'home' : pathname.substr(1)
+    const [activeItem, setActiveItem] = useState('home')
 
   const handleItemClick = (e, { name }) => setActiveItem(name) 
 
   return (
-  <Menu pointing secondary>
+  <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name='home'
         active={activeItem === 'home'}
