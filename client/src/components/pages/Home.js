888746b670d939c589e11382fb4334158f44804e
Rob <robsc801@gmail.com>
2021-06-09 17:29:53 +1200

    getPosts working client side

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,34 @@
 import React from 'react' 
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 function Home() {
-    return (
+    const { loading, data } = useQuery (FETCH_POSTS_QUERY)
+
+    if(data ){
+        console.log(data)
+        }
+     return (
         <div>
             <h1>Home Page</h1>
         </div>
 
         )
     }
+
+const FETCH_POSTS_QUERY = gql`
+    { getPosts{ 
+        id body createdAt username likeCount
+        likes{
+            username
+        }
+        commentCount
+        comments{
+            id username createdAt body
+        }
+    }
+    }
+    `
+
 
     export default Home
