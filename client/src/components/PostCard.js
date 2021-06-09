1e5c665ec00b7b4ae811595b7412342472240a69
Rob <robsc801@gmail.com>
2021-06-09 19:00:00 +1200

    adjust homepage Header

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -37,12 +37,11 @@ function PostCard({
       </Label>
     </Button>
     <Button as='div' labelPosition='right' onClick={commentOnPost}>
-      <Button color='teal' basic>
-        <Icon name='heart' />
-        Like
+      <Button color='blue' basic>
+        <Icon name='comments' />
       </Button>
-      <Label basic color='teal' pointing='left'>
-        {likeCount}
+      <Label basic color='blue' pointing='left'>
+        {commentCount}
       </Label>
     </Button>
           </Card.Content>
