2c0a671afb3a25cf3a7ee12cd5631f901bd0ce21
Rob <robsc801@gmail.com>
2021-06-09 18:51:11 +1200

    create functions for like post and commet on post

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,12 +1,21 @@
 import React from 'react' 
-import { Card, Icon, Label, Image } from 'semantic-ui-react'
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 function PostCard({ 
     post: { body, createdAt, id, username, likeCount, commentCount, likes } 
 }) {
+
+    function likePost(){
+        console.log('Like post!!')
+    }
+
+    function commentOnPost(){
+        console.log('comment on post')
+    }
     return (
-        <Card>
+        <Card fluid>
         <Card.Content>
           <Image
             floated='right'
@@ -14,11 +23,28 @@ function PostCard({
             src="https://react.semantic-ui.com/images/avatar/large/daniel.jpg"
           />
           <Card.Header>{username}</Card.Header>
-          <Card.Meta>{moment(createdAt).fromNow()}</Card.Meta>
+          <Card.Meta as={Link} to={`/posts/${id}`}>{moment(createdAt).fromNow()}</Card.Meta>
           <Card.Description>{body}</Card.Description>
         </Card.Content>
         <Card.Content extra>
-          <p>buttons here</p>
+        <Button as='div' labelPosition='right' onClick={likePost}>
+      <Button color='teal' basic>
+        <Icon name='heart' />
+        Like
+      </Button>
+      <Label basic color='teal' pointing='left'>
+        {likeCount}
+      </Label>
+    </Button>
+    <Button as='div' labelPosition='right' onClick={commentOnPost}>
+      <Button color='teal' basic>
+        <Icon name='heart' />
+        Like
+      </Button>
+      <Label basic color='teal' pointing='left'>
+        {likeCount}
+      </Label>
+    </Button>
           </Card.Content>
         </Card> 
     )
